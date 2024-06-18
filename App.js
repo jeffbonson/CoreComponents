bad85ff03a5d05b85334de8afc291fac10bd560b
jeffbonson <jeffbonson@gmail.com>
2024-06-18 14:41:55 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+const logo = require("./assets/splash.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} style={{width: 300, height: 300}}/>
+      <Image source={{uri: 'https://picsum.photos/300'}} style={{width: 300, height: 300}}/>
+      <ImageBackground source={logo} style={{flex: 1}}>
+        <Text>Background</Text>
+        <Text>Background</Text>
+        <Text>Background</Text>
+        <Text>Background</Text>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, // this is needed.
+    backgroundColor: 'white',
+    borderStyle: "solid",
+    borderWidth: 5,
+    borderColor: "black",
   },
+  inner: {
+    width: 200, height:200, backgroundColor: "lightgreen", justifyContent: "center", alignItems: "center",
+    white: {color: "white", fontWeight: "bold"}
+  }
 });
